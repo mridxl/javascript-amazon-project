00be461bc1dea17e109d5f9860beaf0c72cc77e4
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-05-31 11:00:41 +0530

    Refactor cart.js to use localStorage for cart data

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,20 @@
-export let cart = [
-	{
-		productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-		quantity: 2,
-	},
-	{
-		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-		quantity: 1,
-	},
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+	cart = [
+		{
+			productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+			quantity: 2,
+		},
+		{
+			productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+			quantity: 1,
+		},
+	];
+}
+function savetoStorage() {
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
 	// check if a product is already in cart
@@ -31,6 +38,8 @@ export function addToCart(productId) {
 			quantity,
 		});
 	}
+
+	savetoStorage();
 }
 export function removeFromCart(productId) {
 	const newCart = [];
@@ -38,4 +47,5 @@ export function removeFromCart(productId) {
 		if ((cartItem.productId = productId)) newCart.push(cartItem);
 	});
 	cart = newCart;
+	savetoStorage();
 }

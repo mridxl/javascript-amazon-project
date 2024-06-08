50f43229544ebfa5599b4aac18602ec138cab1fe
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-06-08 16:30:41 +0530

    Refactor loadProductsFetch function to handle error with alert

--- data/products.js
+++ data/products.js
@@ -1,23 +1,16 @@
 export function loadProductsFetch() {
 	const promise = fetch('../backend/products.json')
 		.then((response) => {
+			// Parse the JSON data
 			return response.json();
 		})
-		.then((data) => {
-			// Parse the JSON data
-			const products = data;
-
-			// Log the products to the console
-			return (products);
-		})
 		.catch((error) => {
-			console.log('Unexpected error. Please try again later.', error);
+			alert('Unexpected error. Please try again later.', error);
 		});
 
 	return promise;
 }
 
-// Assuming the loadProductsFetch function is defined somewhere in your code
 
 async function fetchAndProcessProducts() {
 	try {
@@ -25,7 +18,7 @@ async function fetchAndProcessProducts() {
 		return products;
 		
 	} catch (error) {
-		console.log('An error occurred:', error);
+		alert('An error occurred:', error);
 	}
 }
 
